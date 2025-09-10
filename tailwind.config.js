c339f3676fc445aad68dd35db5b31b20a3444c18
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-09-10 15:35:40 +0530

    completed project set up

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5B21B6",
+        accent: "#06B6D4",
+      },
+    },
+  },
+  plugins: [],
+};
